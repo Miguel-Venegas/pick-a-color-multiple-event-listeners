89b6b1767d32225ee17368167d0c192371ee83e0
Miguel-Venegas <venegas.a.miguel@gmail.com>
2021-01-01 07:37:20 -0800

    pick a color lesson

---
+++ app.js
@@ -0,0 +1,30 @@
+const colors = [
+	'red',
+	'orange',
+	'yellow',
+	'green',
+	'blue',
+	'purple',
+	'indigo',
+	'violet'
+];
+
+const h1 = document.querySelector('h1');
+
+const changeColor = function () {
+	h1.style.color = this.style.backgroundColor;
+	console.log(this.style.backgroundColor);
+}
+
+const container = document.querySelector('#boxes');
+
+
+for(let color of colors){
+	const box = document.createElement('div');
+	box.style.backgroundColor = color;
+	box.classList.add('box');
+	container.appendChild(box);
+	box.addEventListener('click', changeColor)
+}
+
+
